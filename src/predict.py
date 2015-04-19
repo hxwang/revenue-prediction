b81ef8a2265873dcc60a336bee28bdd7f81ac500
Huangxin <maggiewangsjtu@gmail.com>
2015-04-19 15:51:25 -0400

    add classification

--- src/predict.py
+++ src/predict.py
@@ -4,6 +4,7 @@ import sys
 import math
 import numpy as np
 import smote
+import random
 from sklearn import svm
 from sklearn import cross_validation
 from sklearn import kernel_ridge
@@ -12,6 +13,10 @@ from sklearn.metrics import mean_squared_error
 from sklearn.neighbors import *
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import *
+import matplotlib.pyplot as plt
+from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import fbeta_score, make_scorer
+from sklearn.preprocessing import *
 
 
 '''
@@ -39,6 +44,47 @@ def write_solution(filename, y):
             f.write(str(i)+','+str(y[i])+'\n')
     print 'done'
 
+
+'''
+return model
+'''
+def train_classcification_model(X, y, threshold = 9e6, config={}):
+    labels = binarize(y, split_point)[0]
+    print X.shape
+    print labels.shape
+
+    n = len(X)
+    k = 3
+    shuffle = True
+
+    # k fold
+    kf = cross_validation.KFold(n=n, n_folds=k, shuffle=shuffle)
+
+    for train_index, test_index in kf:
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = labels[train_index], labels[test_index]
+
+        model = svm.SVC()        
+        #model = KNeighborsClassifier(n_neighbors = 5, weights='distance')
+        model.fit(X_train, y_train)
+
+        predicted = model.predict(X_test)
+
+        if 'figure' in config:
+            visualize_predict(predicted, y_test) 
+
+    # train with all data 
+    model = svm.SVC()
+    model.fit(X, y)
+    return model       
+
+def classifify_data(X_test, model):
+    y_test_label = model.predict(X_test)
+
+def predict_test_by_class(X_test, y_test_label, model):
+
+
+
 '''
 produce model 
 X: record, n*m matrix
@@ -59,13 +105,15 @@ def fit(X, y, config = {}):
             
             ########################################################################## 
             # for standardized data
-           
+            # SGDClassifier(loss="huber")
 
             ###########################################################################
-            KNeighborsRegressor(n_neighbors=22, weights = 'distance'),
-            svm.NuSVR(nu=0.27, C=2.3e7, degree=2, gamma=0.0047),
-            #svm.NuSVR(nu=0.33, C=9e6, degree=2, gamma=0.0092),
-            GradientBoostingRegressor(n_estimators=100, learning_rate=0.15, max_depth=1, random_state=1, loss='lad'),
+            
+            # KNeighborsRegressor(n_neighbors=22, weights = 'distance'),
+            # svm.NuSVR(nu=0.27, C=2.3e7, degree=2, gamma=0.0047),
+
+            # svm.NuSVR(nu=0.33, C=9e6, degree=2, gamma=0.0092),
+            # GradientBoostingRegressor(n_estimators=100, learning_rate=0.15, max_depth=1, random_state=1, loss='lad')
 
             ########################################################################## 
             # for standardized pcaed data
@@ -84,36 +132,36 @@ def fit(X, y, config = {}):
             ###########################################################################
 
             # RandomForestRegressor(n_estimators=100, n_jobs=-1)
-            #AdaBoostRegressor(n_estimators=100,  learning_rate = 0.3, loss='exponential')
-            #GradientBoostingRegressor(n_estimators=100, learning_rate=0.5, max_depth=3, random_state=1, loss='lad'),
-            #GradientBoostingRegressor(n_estimators=150, learning_rate=0.2, max_depth=3, random_state=1, loss='lad')
-            #svm.SVR(C=1.3, degree=3, gamma=0.05)
+            # AdaBoostRegressor(n_estimators=100,  learning_rate = 0.3, loss='exponential')
+            # GradientBoostingRegressor(n_estimators=100, learning_rate=0.5, max_depth=3, random_state=1, loss='lad'),
+            # GradientBoostingRegressor(n_estimators=150, learning_rate=0.2, max_depth=3, random_state=1, loss='lad')
+            # svm.SVR(C=1.3, degree=3, gamma=0.05)
         ]
 
         if 'huahua' in config:
             models = [
-                KNeighborsRegressor(n_neighbors=23, weights='distance'),                
-                svm.NuSVR(nu=0.25, C=1.5e7, degree=2, gamma=0.0042),
-                GradientBoostingRegressor(n_estimators=100, learning_rate=0.5, max_depth=1, random_state=0, loss='lad'),
+                KNeighborsRegressor(n_neighbors=22, weights='distance'),            
+                svm.NuSVR(nu=0.6, C=1e2, degree=2, gamma=0.16),
+                GradientBoostingRegressor(n_estimators=100, learning_rate=0.7, max_depth=1, random_state=0, loss='lad'),
             ]
     else:
 
-        # model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=1, random_state=1, loss='lad')
+        model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.7, max_depth=1, random_state=0, loss='lad')
 
         # model =  RandomForestRegressor(n_estimators=70, n_jobs=-1)
 
-        #  model = KNeighborsRegressor(n_neighbors=22, weights = 'distance')
+        #model = KNeighborsRegressor(n_neighbors=22, weights = 'distance')
         # model = svm.NuSVR(nu=0.25, C=2.3e7, degree=2, gamma=0.0047)
         # #svm.NuSVR(nu=0.33, C=9e6, degree=2, gamma=0.0092),
         # model = GradientBoostingRegressor(n_estimators=80, learning_rate=0.2, max_depth=1, random_state=1, loss='lad')
 
         # # avg:  2.36456772543 total_var: 21.5326576549
         # model = BaggingRegressor(n_estimators=100, n_jobs=-1, max_features = 15)
-        # model = BaggingRegressor(n_estimators=100, n_jobs=-1, max_features = 15)
+        #model = BaggingRegressor(n_estimators=100, n_jobs=-1, max_features = 15)
 
         # model = RandomForestRegressor(n_estimators=10, n_jobs=-1)
         # model = svm.NuSVR(nu=0.27, C=1.6e7, degree=2, gamma=0.0076)
-        # model = svm.NuSVR(nu=0.33, C=9e6, degree=2, gamma=0.0092)
+        # model = svm.NuSVR(nu=0.6, C=1e2, degree=2, gamma=0.16)
         # model = svm.NuSVR(nu=0.27, C=2.3e7, degree=2, gamma=0.0047)
 
         #model = RadiusNeighborsRegressor(radius=15, weights='distance')
@@ -133,21 +181,31 @@ def fit(X, y, config = {}):
 
         # score: 2.43654929108 weights = uniform
         # score: 2.37613424202 n_neighbors=20, weights = distance, test: 1730840.19422
-        # model = KNeighborsRegressor(n_neighbors=28, weights = 'distance')
+        # model = KNeighborsRegressor(n_neighbors=20, weights = 'distance')
 
         # score: 3.40178270362
         # model = DecisionTreeRegressor()
 
         # score: 2.54716252715
-        # model = svm.SVR(C=0.01, degree=3, gamma=0.0000025)
+        #model = svm.SVR(C=1e7, degree=3, gamma=0.0000025)
         # total avg:  2.55070552804 total_var: 37.7768440307
-        model = svm.SVR(kernel='poly', coef0 = 60, C=6, degree=2, gamma=0.004)
+
+        # Grid Search
+        # scorer = make_scorer(mean_squared_error, greater_is_better=False)
+        # grid = GridSearchCV(LinearSVC(), param_grid={'C': [1, 10]}, scoring=scorer)
+
+        # model = svm.SVR(kernel='poly', coef0 = 60, C=6, degree=2, gamma=0.004)
 
         models = [ model ]
 
     # oversampling the training set
     if 'oversampling' in config:
         X, y = smote.SMOTE(X, y, 200, 20)
+
+    if 'logy' in config:
+        y = np.log(y)
+
+
 
     for model in models:
         print 'fitting model...'
@@ -161,7 +219,7 @@ models: given models
 X: record to predict, n*m matrix
 return: predicted (mean) revenue, n*1 matrix
 '''
-def predict(models, X):
+def predict(models, X, config={}):
 
     ys = []
 
@@ -171,6 +229,9 @@ def predict(models, X):
 
     # return mean prediction
     y = np.mean(np.array(ys), axis=0)
+
+    if 'logy' in config:
+        y = np.exp(y)
 
     return y
 
@@ -193,8 +254,13 @@ def k_folds(X, y, k = 5, config={}):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         models = fit(X_train, y_train, config)
-        y_predict = predict(models, X_test)
 
+        y_predict = predict(models, X_test, config)
+
+        # if 'figure' in config:
+        #     # visualize predict results
+        #     visualize_predict(y_predict, y_test)
+       
         score = math.sqrt(mean_squared_error(y_predict, y_test))
         print 'score: ', score/ 1e6
         # total_score += score
@@ -209,17 +275,55 @@ def k_folds(X, y, k = 5, config={}):
 
     return total_score_arr
 
+'''
+visualize predict results
+'''
+def visualize_predict(y_predict, y_test):
+    X = [i for i in range(0, len(y_predict))]
+    print 'X', X
+
+    fig = plt.gcf()
+    fig.set_size_inches(18.5,10.5)
+    plt.scatter(X, y_test, c='k', label='data')
+    plt.plot(X, y_predict, c='g', label='prediction')
+    plt.axis('tight')
+    plt.legend(loc='center left')
+    plt.title("Predict v.s. Real")
+    plt.show()
+
 def predict_test(X_train, y_train, X_test, config):
     
     # rebuild the model
 
     models = fit(X_train, y_train, config)
-    y_predict = predict(models, X_test)
+    y_predict = predict(models, X_test, config)
 
     solution_filename = 'solution.csv'
     print 'y_predict-share', y_predict.shape
 
     write_solution(solution_filename, y_predict)
+
+# resample towards high revenue
+def resample(X_train, y_train,  threshold = 9e6, ratio = 0.2):
+
+    X = []
+    y = []
+
+
+    # prob = normalize(np.absolute(scale(y_train)))[0]
+
+    # print prob
+
+    while len(X) < len(X_train) * 2:
+        for i in range(0, len(y_train)):
+            p = random.random()
+            if y_train[i]<8e6 and p > ratio:
+                continue
+            else:
+                X.append(X_train[i])
+                y.append(y_train[i])
+
+    return np.array(X), np.array(y)
 
 def parse_arg(argv):
     config = {}
@@ -260,6 +364,12 @@ def parse_arg(argv):
             config['huahua'] = True
         if arg == '-kfolds':
             config['kfolds'] = int(argv[i+1])
+        if arg == '-logy':
+            config['logy'] = True
+        if arg == '-figure':
+            config['figure'] = True
+        if arg == '-resample':
+            config['resample'] = True
     return config
 
 if __name__ == '__main__':
@@ -297,7 +407,19 @@ if __name__ == '__main__':
     # split training data to (record) and (predicted value)
     X_train = train_data[:, cols]
     y_train = train_data[:, len(train_data[0])-1]
+
+
+    # settings
+    class_split_threshold =  9e6
+
+    #resample
+    if('resample' in config):
+        X_train, y_train = resample(X_train, y_train, threshold = class_split_threshold)
+
+    train_classcification_model(X_train, y_train, threshold = class_split_threshold,  config = config)
     
+   
+
     # calcualte average Root Mean Squared Eror (RMSE)
     total_avg = 0.0
 
